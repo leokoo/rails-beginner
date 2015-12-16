5646b0c98ef810623b25f0faa39de705071ca2ce
Leo Koo <leokoo@gmail.com>
2015-12-16 11:35:08 +0800

    Adding errors notification

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,8 @@
 class ArticlesController < ApplicationController
+def index
+	@articles = Article.all
+end
+
 def show
 	@article = Article.find(params[:id])
 end
@@ -8,9 +12,11 @@ end
 
 def create
 	@article = Article.new(article_params)
-
-	@article.save
-	redirect_to @article
+	if @article.save
+		redirect_to @article
+	else
+		render 'new'
+	end
 end
 
 private
